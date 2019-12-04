fb6dff8d69e7c81f8afbd481ae71f75e0c1cf428
dmanick2 <dmanick@nldford.fbp2.ford.com>
2019-12-04 12:23:14 -0500

    sagas

--- src/sagas/post.js
+++ src/sagas/post.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeEvery, call, put, select } from "redux-saga/effects";
 import { GET_POST, ADD_POST } from "../constants/constants";
 import { getPostService } from "../services/postService";
 
@@ -6,9 +6,15 @@ export default function* watcherSaga() {
   yield takeEvery(GET_POST, workerSaga);
 }
 
-function* workerSaga() {
+function* workerSaga(action) {
   try {
-    const payload = yield call(getPostService);
+    const thingsFromReduxStore = yield select(state => state.article.articles);
+
+    console.log("redux store's state access :", thingsFromReduxStore);
+    console.log("Action can be accessed : ", action.type);
+    console.log("Passing a dummy arg from the saga to the service method...");
+
+    const payload = yield call(() => getPostService(action.type));
     yield put({ type: ADD_POST, payload });
   } catch (e) {
     yield put({ type: "API_ERRORED", payload: e });
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-export const getPostService = async () => {
+export const getPostService = async arg => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts"
   );
+  console.log("Demonstrates that args can be pased :", arg);
   return data;
 };
